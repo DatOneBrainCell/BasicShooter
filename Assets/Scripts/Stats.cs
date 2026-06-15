f76ced4ae4170e5b14265285185120e9afad63ef
DatOneBraincell <crjasperdaniel@gmail.com>
2026-06-16 00:32:11 +0530

    Added death animation

--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Stats : MonoBehaviour
@@ -5,16 +7,27 @@ public class Stats : MonoBehaviour
     [SerializeField] private int maxHealth;
 
     [SerializeField] private int health;
+    [SerializeField] private float despawnTime;
+
+    private Animator animator;
+    private Collider collider;
+    private EnemyBehaviour enemyBehaviour;
+
+    private const string IS_DYING = "IsDying";
 
     private void Start() {
 
         health = maxHealth;
+
+        animator = transform.GetChild(0).GetComponent<Animator>();
+        collider = GetComponent<BoxCollider>();
+        enemyBehaviour = GetComponent<EnemyBehaviour>();
     }
 
     internal void Damage(int dmgAmt) {
 
         if (health <= 0 || dmgAmt >= health) {
-            Destroy(gameObject);
+            StartCoroutine(DestroyGameObject());
         }
         else {
             health -= Random.Range(dmgAmt - 7, dmgAmt + 5);
@@ -28,6 +41,28 @@ public class Stats : MonoBehaviour
         }
         else {
             health += healAmt;
+        }
+    }
+
+    IEnumerator DestroyGameObject() {
+
+        if (animator != null) {
+
+            animator.SetBool(IS_DYING, true);
+
+            enemyBehaviour.enabled = false;
+
+            if (collider  != null) {
+                collider.enabled = false;
+            }
+
+            yield return new WaitForSeconds(despawnTime);
+
+            Destroy(gameObject);
+        }
+
+        else {
+            Destroy(gameObject);
         }
     }
 }

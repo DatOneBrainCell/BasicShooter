69c36453a2679b109c9c2d51619958e650f51761
DatOneBraincell <crjasperdaniel@gmail.com>
2026-06-24 20:34:59 +0530

    Changed the restart button

--- Assets/Scripts/MainMenu/SceneLoader.cs
+++ Assets/Scripts/MainMenu/SceneLoader.cs
@@ -1,12 +1,19 @@
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    private const string MAIN_MENU = "MainMenu";
+    private const string SAMPLE_SCENE = "SampleScene";
+
     public void LoadGameScene() {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene(SAMPLE_SCENE);
     }
     public void QuitApplication() {
         Application.Quit();
+    }
+    public void GoToMainMenu() {
+        SceneManager.LoadScene(MAIN_MENU);
     }
 }
